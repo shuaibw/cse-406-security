6eff20b3dcab627ef3451f46c7cac73b73eff751
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2023-06-29 00:24:33 +0600

    Implement RSA auth

--- offline1/rsa.py
+++ offline1/rsa.py
@@ -36,28 +36,53 @@ def generate_rsa_key_pairs(n=2048):
 
 def rsa_encrypt(m, public_key):
     """
-    m: message to be encrypted
+    m: string to be encrypted
     pub: public key pair (e,n)
-    returns: encrypted message
+    returns: encrypted message as integer
     """
+    e, n = public_key
+    return fast_modular_exp(str_to_int(m), e, n)
+
+
+def rsa_encrypt_raw(m, public_key):
     e, n = public_key
     return fast_modular_exp(m, e, n)
 
 
 def rsa_decrypt(c, private_key):
     """
-    c: message to be decrypted
+    c: integer to be decrypted
     private_key: private key pair (d,n)
-    returns: decrypted message
+    returns: decrypted message as string
     """
+    d, n = private_key
+    return int_to_str(fast_modular_exp(c, d, n))
+
+
+def rsa_decrypt_raw(c, private_key):
     d, n = private_key
     return fast_modular_exp(c, d, n)
 
 
 if __name__ == "__main__":
-    public, private = generate_rsa_key_pairs()
-    msg = "Hello from the other side of the wall"
-    cipher = rsa_encrypt(str_to_int(msg), public)
-    print("Cipher: ", cipher)
-    deciphered = rsa_decrypt(cipher, private)
-    print("Deciphered: ", int_to_str(deciphered))
+    # ------------------ Basic Test ------------------
+    # public, private = generate_rsa_key_pairs()
+    # msg = "1"
+    # cipher = rsa_encrypt(msg, public)
+    # print("Cipher: ", cipher)
+    # deciphered = rsa_decrypt(cipher, private)
+    # print("Deciphered: ", deciphered)
+    # ------------------ Basic Test ------------------
+    
+    # ------------------ Auth Test ------------------
+    sender_public, sender_private = generate_rsa_key_pairs()
+    receiver_public, receiver_private = generate_rsa_key_pairs()
+    msg = "Hello world!"
+    cipher = rsa_encrypt(msg, receiver_public)
+    signed = rsa_encrypt_raw(cipher, sender_private)
+    recieved = signed
+    deciphered = rsa_decrypt_raw(recieved, sender_public)
+    deciphered = rsa_decrypt(deciphered, receiver_private)
+    print(deciphered)
+    
+    # ------------------ Auth Test ------------------
--- offline1/util.py
+++ offline1/util.py
@@ -1,3 +1,6 @@
+import random
+
+
 def fast_modular_exp(base, exponent, modulus):
     if modulus == 1:
         return 0
@@ -10,6 +13,43 @@ def fast_modular_exp(base, exponent, modulus):
             exponent = exponent >> 1
             base = (base * base) % modulus
         return result
+
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def find_coprime(n):
+    """
+    Returns a number coprime to n
+    """
+    while True:
+        e = random.randint(2, n-1)
+        if gcd(e, n) == 1:
+            return e
+
+
+def egcd(a: int, b: int):
+    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        (q, a), b = divmod(b, a), a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+
+def fast_modular_inverse(a, m):
+    """
+    Returns the modular inverse of a modulo m
+    """
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception("No modular inverse found")
+    return x % m
+
 
 if __name__ == "__main__":
     from diffie_hellman import *
@@ -32,9 +72,6 @@ if __name__ == "__main__":
         ans2 = pow(a, b, c)
         toc = time.perf_counter_ns()
         times["pow"] += (toc - tic) / 10**6
-        assert ans1==ans2
+        assert ans1 == ans2
 
     print(times)
-        
-    
-
