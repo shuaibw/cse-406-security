fbc1b3aa6dc1c35becff9ebc0a92fd81b8d09693
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2023-06-28 22:18:06 +0600

    Add tests

--- offline1/diffie_hellman.py
+++ offline1/diffie_hellman.py
@@ -1,5 +1,8 @@
 import random
 from util import *
+import time
+from prettytable import PrettyTable
+
 
 """
 ---------------Diffie-Hellman Key Exchange----------------
@@ -114,10 +117,12 @@ def generate_modulus_and_base(key_size=128, prime_fct_bit=32, samples=1000, min=
     min, max: range of the candidate primitive roots
     and base is a primitive root of modulus
     """
+    assert key_size % prime_fct_bit == 0
+    assert min < max <= 2**key_size
     MIN = min
     MAX = max
     prime_list = [generate_large_prime(prime_fct_bit) for _ in range(samples)]
-    factor_count = key_size // prime_fct_bit
+    factor_count = key_size // prime_fct_bit  # number of prime factors to be used in the modulus
     modulus, base = find_base_and_modulus(prime_list, key_size, factor_count)
     return modulus, base
 
@@ -142,19 +147,117 @@ def generate_shared_secret(modulus, public_key, private):
     return fast_modular_exp(public_key, private, modulus)
 
 
-if __name__ == '__main__':
-    modulus, base = generate_modulus_and_base(
-        key_size=128, prime_fct_bit=32, samples=1500, min=2**64, max=2**128)
-    print("Public Modulus (p):", modulus)
-    print("Public Base (g):", base)
-    print("Number of bits in p:", modulus.bit_length())
-    print("Number of bits in g:", base.bit_length())
+def __find_base(prime_list, key_size, factor_count):
+    count = 0
+    while True:
+        factors = random.sample(prime_list, factor_count)
+        factors.append(2)
+        p = 1
+        for factor in factors:
+            p *= factor
+        p += 1
+        if p.bit_length() != key_size:
+            continue
+        if miller_rabin(p):
+            return p, factors
+        count += 1
+        if (count % 100000 == 0):
+            print("Attempt: ", count)
 
-    sender_private = generate_large_prime(128)
-    receiver_private = generate_large_prime(128)
-    sender_public = generate_public_key(modulus, base, sender_private)
-    receiver_public = generate_public_key(modulus, base, receiver_private)
-    sender_shared_secret = generate_shared_secret(modulus, receiver_public, sender_private)
-    receiver_shared_secret = generate_shared_secret(modulus, sender_public, receiver_private)
-    print("Sender's shared secret:", sender_shared_secret)
-    print("Receiver's shared secret:", receiver_shared_secret)
+
+def __run_test(key_sizes, prime_fct_bit, prime_list, min, max, iter=10):
+    MIN = min
+    MAX = max
+    times = {
+        128: {
+            'p': 0,
+            'g': 0,
+            'a': 0,
+            'A': 0,
+            'secret': 0
+        },
+        192: {
+            'p': 0,
+            'g': 0,
+            'a': 0,
+            'A': 0,
+            'secret': 0
+        },
+        256: {
+            'p': 0,
+            'g': 0,
+            'a': 0,
+            'A': 0,
+            'secret': 0
+        }
+    }
+    for _ in range(iter):
+        print('Iteration: ', _)
+        for k in key_sizes:
+            print(f'Running test for Key size: {k}')
+            tic = time.perf_counter_ns()
+            p, factors = __find_base(prime_list, k, k//prime_fct_bit)
+            toc = time.perf_counter_ns()
+            times[k]['p'] += (toc-tic)/10**6
+
+            tic = time.perf_counter_ns()
+            modulus = find_primitive_root(p, factors)
+            toc = time.perf_counter_ns()
+            times[k]['g'] += (toc-tic)/10**6
+
+            tic = time.perf_counter_ns()
+            a = generate_large_prime(k)
+            toc = time.perf_counter_ns()
+            times[k]['a'] += (toc-tic)/10**6
+
+            b = generate_large_prime(k)
+
+            tic = time.perf_counter_ns()
+            A = fast_modular_exp(modulus, a, p)
+            toc = time.perf_counter_ns()
+            times[k]['A'] += (toc-tic)/10**6
+
+            B = fast_modular_exp(modulus, b, p)
+
+            tic = time.perf_counter_ns()
+            secret_A = fast_modular_exp(B, a, p)
+            toc = time.perf_counter_ns()
+            times[k]['secret'] += (toc-tic)/10**6
+
+            secret_B = fast_modular_exp(A, b, p)
+            assert secret_A == secret_B
+    return times
+
+
+if __name__ == '__main__':
+    # ----------------- Testing -----------------
+    # modulus, base = generate_modulus_and_base(
+    #     key_size=128, prime_fct_bit=32, samples=1500, min=2**64, max=2**128)
+    # print("Public Modulus (p):", modulus)
+    # print("Public Base (g):", base)
+    # print("Number of bits in p:", modulus.bit_length())
+    # print("Number of bits in g:", base.bit_length())
+
+    # sender_private = generate_large_prime(128)
+    # receiver_private = generate_large_prime(128)
+    # sender_public = generate_public_key(modulus, base, sender_private)
+    # receiver_public = generate_public_key(modulus, base, receiver_private)
+    # sender_shared_secret = generate_shared_secret(modulus, receiver_public, sender_private)
+    # receiver_shared_secret = generate_shared_secret(modulus, sender_public, receiver_private)
+    # print("Sender's shared secret:", sender_shared_secret)
+    # print("Receiver's shared secret:", receiver_shared_secret)
+    # ----------------- Testing -----------------
+    key_sizes = [128, 192, 256]
+    iter = 20
+    primes = [generate_large_prime(32) for _ in range(2500)]
+    times = __run_test(key_sizes, 32, primes, 2**64, 2**128, iter)
+    table = PrettyTable()
+    table.field_names = ["Key Size", "p", "g", "a", "A", "Secret"]
+    for k, v in times.items():
+        p = "{:.5f}".format(v['p']/iter)
+        g = "{:.5f}".format(v['g']/iter)
+        a = "{:.5f}".format(v['a']/iter)
+        A = "{:.5f}".format(v['A']/iter)
+        secret = "{:.5f}".format(v['secret']/iter)
+        table.add_row([k, p, g, a, A, secret])
+    print(table)
